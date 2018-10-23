771eb56577d3347ff02f0d4d88b8a2c744037b90
jkvora <vorajalak@yahoo.co.in>
2018-10-23 10:01:47 +0530

    Create module and simple component strcuture

---
+++ index.ts
@@ -0,0 +1,2 @@
+export { LazyLoadImagesModule } from './lib/lazy-load-images.module';
+export { LazyLoadImage } from './lib/lazy-load-image/lazy-load-image.component'
---
+++ lib/lazy-load-image/lazy-load-image.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'lazyimg',
+    templateUrl: './lazy-load-image.component.html',
+    styleUrls: ['./lazy-load-image.component.css']
+})
+export class LazyLoadImage {
+
+
+}
