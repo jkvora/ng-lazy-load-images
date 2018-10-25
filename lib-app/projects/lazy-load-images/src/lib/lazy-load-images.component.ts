28376b8a5efda76af119e0bc5a50365118c0cdce
jkvora <vorajalak@yahoo.co.in>
2018-10-25 09:56:42 +0530

    Basics cleared

--- lib-app/projects/lazy-load-images/src/lib/lazy-load-images.component.ts
+++ lib-app/projects/lazy-load-images/src/lib/lazy-load-images.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'ngx-lazy-load-image',
-  template: `
-    <p>
-      lazy-load-images works!
-    </p>
-  `,
+  templateUrl: 'lazy-load-images.component.html',
   styles: []
 })
-export class LazyLoadImagesComponent implements OnInit {
+export class LazyLoadImagesComponent implements OnInit,OnChanges {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes:SimpleChanges){
+    console.log("Changes are:",changes);
   }
 
 }
--- lib-app/src/app/app.component.ts
+++ lib-app/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-demo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'lib-app';
+export class AppComponent implements OnInit{
+  name = 'Jalak';
+
+
+  ngOnInit (){
+      setTimeout(() => {
+          this.name="Vora "
+      }, 2000);
+  }
 }
